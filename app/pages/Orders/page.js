44edd55a0f01f9c51c8eb18c33f1b593028ed99a
Vo Khanh Linh <vokhanhlinh@Vos-Mac-mini.local>
2024-01-25 02:12:04 +0900

    o

---
+++ app/pages/Orders/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Orders = () => {
+  return (
+    <div>
+     Oders
+    </div>
+  )
+}
+
+export default Orders
+
---
+++ app/pages/unauth-page/page.js
@@ -0,0 +1,8 @@
+
+
+
+export default function UnauthPage(){
+    return <div>
+        Chua dang nhap
+    </div>
+}
